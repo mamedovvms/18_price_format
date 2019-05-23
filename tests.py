f048cd3db5d3913fa0ebb9006e30d15edaf9666d
1237 <mamedov_vs@oksw.ru>
2019-05-23 14:28:19 +0700

    Созданы тесты для проверки функции форматирования цены.

--- tests.py
+++ tests.py
@@ -1,1 +1,18 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestPrice(unittest.TestCase):
+    def test_no_fractional(self):
+        self.assertEqual(format_price('3245.000000'), '3 245')
+
+    def test_from_fractional(self):
+        self.assertEqual(format_price('123456.7890'), '123 456,789')
+
+    def test_incorrect_value(self):
+        self.assertIsNone(format_price('Not price'))
+        self.assertIsNone(format_price('-10'))
+
+
+if __name__ == '__main__':
+    unittest.main()
