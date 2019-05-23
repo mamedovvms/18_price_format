c20677028fd83b403507b4b96820c9ddea5efb89
1237 <mamedov_vs@oksw.ru>
2019-05-23 18:23:00 +0700

    Разбиты тесты.

--- tests.py
+++ tests.py
@@ -4,23 +4,34 @@ from format_price import format_price
 
 class TestPrice(unittest.TestCase):
     def test_no_fractional(self):
-        self.assertEqual(format_price('3245.00000'), '3 245')
         self.assertEqual(format_price('3245'), '3 245')
+
+    def test_zero_fractional(self):
+        self.assertEqual(format_price('3245.00000'), '3 245')
+
+    def test_smal_value_no_fractional(self):
         self.assertEqual(format_price('5'), '5')
 
     def test_from_fractional(self):
         self.assertEqual(format_price('123456.7890'), '123 456.79')
+
+    def test_smal_value_fractional(self):
         self.assertEqual(format_price('12.889'), '12.89')
+
+    def test_round_result(self):
         self.assertEqual(format_price('12.999'), '13')
 
     def test_negative_value(self):
         self.assertEqual(format_price('-10000'), '-10 000')
+
+    def test_negative_smal_value(self):
         self.assertEqual(format_price('-111.789'), '-111.79')
 
-    def test_incorrect_value(self):
+    def test_incorrect_value_not_number(self):
         self.assertIsNone(format_price('Not price'))
+
+    def test_incorrect_value_separator(self):
         self.assertIsNone(format_price('10000,00'))
-        self.assertIsNone(format_price('10000,12'))
 
 
 if __name__ == '__main__':
