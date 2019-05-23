dc06c9938f2a3a3ac2f4bebc874c78db729852f5
1237 <mamedov_vs@oksw.ru>
2019-05-23 16:58:50 +0700

    Добавлены проверки для отрицательных значений, для неправильных значений, добавлены на округление

--- tests.py
+++ tests.py
@@ -4,14 +4,23 @@ from format_price import format_price
 
 class TestPrice(unittest.TestCase):
     def test_no_fractional(self):
-        self.assertEqual(format_price('3245.000000'), '3 245')
+        self.assertEqual(format_price('3245.00000'), '3 245')
+        self.assertEqual(format_price('3245'), '3 245')
+        self.assertEqual(format_price('5'), '5')
 
     def test_from_fractional(self):
-        self.assertEqual(format_price('123456.7890'), '123 456,789')
+        self.assertEqual(format_price('123456.7890'), '123 456.79')
+        self.assertEqual(format_price('12.889'), '12.89')
+        self.assertEqual(format_price('12.999'), '13')
+
+    def test_negative_value(self):
+        self.assertEqual(format_price('-10000'), '-10 000')
+        self.assertEqual(format_price('-111.789'), '-111.79')
 
     def test_incorrect_value(self):
         self.assertIsNone(format_price('Not price'))
-        self.assertIsNone(format_price('-10'))
+        self.assertIsNone(format_price('10000,00'))
+        self.assertIsNone(format_price('10000,12'))
 
 
 if __name__ == '__main__':
