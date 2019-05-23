b0cab9ddac5dce1969d419f8f64acc491402f685
1237 <mamedov_vs@oksw.ru>
2019-05-23 14:27:51 +0700

    Создана функция форматирования цены.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,37 @@
+import argparse
+
+
+def get_cmd_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price', help='Price for formatting')
+    return parser.parse_args()
+
+
 def format_price(price):
-    pass
+    try:
+        float_price = float(price)
+    except ValueError:
+        return None
+
+    if float_price < 0:
+        return None
+
+    integer = int(float_price)
+    result = '{:,}'.format(integer).replace(',', ' ')
+
+    if not float_price == integer:
+        integer_chunk, *fractional_chunk = price.split('.')
+        fractional = fractional_chunk[0].rstrip('0')
+        result += ',{}'.format(int(fractional))
+
+    return result
+
+
+def main():
+    params = get_cmd_params()
+    price = params.price
+    print(format_price(price))
+
 
 if __name__ == '__main__':
-    pass
+    main()
