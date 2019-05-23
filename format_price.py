0112875a4b9d35eb6f2744ac5eddba2b6c314f2c
1237 <mamedov_vs@oksw.ru>
2019-05-23 16:56:16 +0700

    Изменена функция форматирования. Применен один и тот же подход к обоим случаям. Удалена проверка отрицательного значения. Добавлено округление значения.

--- format_price.py
+++ format_price.py
@@ -13,17 +13,14 @@ def format_price(price):
     except ValueError:
         return None
 
-    if float_price < 0:
-        return None
+    float_price = float(price)
+    round_float_price = float('{:.2f}'.format(float_price))
+    integer_price = int(round_float_price)
 
-    integer = int(float_price)
-    result_price = '{:,}'.format(integer).replace(',', ' ')
-
-    if not float_price == integer:
-        integer_chunk, *fractional_chunk = price.split('.')
-        fractional = fractional_chunk[0].rstrip('0')
-        result_price += ',{}'.format(int(fractional))
-
+    if round_float_price == integer_price and int(float_price):
+        result_price = '{:,}'.format(integer_price).replace(',', ' ')
+    else:
+        result_price = '{:,.2f}'.format(round_float_price).replace(',', ' ')
     return result_price
 
 
